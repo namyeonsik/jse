2e0984e1fbd78770188810802201ad563b79cf88
user <user@192.168.10.36>
2016-02-15 15:21:06 +0900

    time calc

---
+++ jse/src/operation/TimeCalc.java
@@ -0,0 +1,24 @@
+package operation;
+
+import java.util.Scanner;
+
+public class TimeCalc {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("*** 초를 시간과 분과 초로 바꾸어 주는 프로그램 ***");
+		/**
+		 * + - * / % 
+		 * 
+		 * sec, hour, min
+		 * */
+		System.out.println("초를 입력하세요.");
+		int sec = sc.nextInt();
+		int sec2 = sec;
+		int min = sec / 60;
+		int hour = min / 60;
+		min %= 60;
+		sec %= 60;
+		System.out.println("입력한 " + sec2 + "초는 " + hour + "시간 " + min + "분 " + sec + "초입니다.");
+	}
+}
