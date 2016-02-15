4ebe09957f8c4db1a8ef8595044fbf4146834b70
user <user@192.168.10.36>
2016-02-15 17:17:20 +0900

    postfix

---
+++ jse/src/unary/Postfix.java
@@ -0,0 +1,10 @@
+package unary;
+
+public class Postfix {
+	public static void main(String[] args) {
+		int i = 10, j = 10;
+		System.out.println(++i);	// i + 1
+		System.out.println(--j);	// i + 1
+		
+	}
+}
