56256c53393a5892cfe06a78ce60c6fc1a7a20f1
user <user@192.168.10.36>
2016-02-15 14:55:19 +0900

    report card

--- jse/src/operation/ReportCard.java
+++ jse/src/operation/ReportCard.java
@@ -8,7 +8,7 @@ public class ReportCard {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("학생이름 :");
 		String name = sc.next();
-		
+
 		System.out.println("국어 점수 :");
 		int kor = sc.nextInt();
 		System.out.println("영어 점수 :");
@@ -17,10 +17,18 @@ public class ReportCard {
 		int math = sc.nextInt();
 		System.out.println("과학 점수 :");
 		int sci = sc.nextInt();
+
+		int total = kor + eng + math + sci;
+		double avg = total / 4;
+		String result = name + "학생의 총점은 " + total + "점 이고, 평균은 " + avg + "점으로 ";
 		
-		int total = kor+eng+math+sci;
-		System.out.println(name + "학생의 총점은 " + total + "점 이고, 평균은 " + total/4 + "점 입니다.");
-				
-				
+		if (avg >= 90.0) {
+			System.out.println(result + "장학생입니다.");
+		} else if (avg >= 60.0) {
+			System.out.println(result + "합격입니다.");
+		} else {
+			System.out.println(result + "불합격입니다.");
+		}
+
 	}
 }
