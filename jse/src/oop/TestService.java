d74ff86d9fd16a72f2fff6412f3e9e1c59c6509f
user <user@user-PC>
2016-02-16 13:09:34 +0900

    Kaup interface, main, class

---
+++ jse/src/oop/TestService.java
@@ -0,0 +1,8 @@
+package oop;
+
+public interface TestService {
+	public String getState1(double height, double weight);
+	public String getState2(double height, double weight);
+	public String getState3(double height, double weight);
+	public String getState4(double height, double weight);
+}
